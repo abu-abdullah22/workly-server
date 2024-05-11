ccf42edf50a2ceb50db90e9df3ad2270baee8120
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-05-11 18:21:07 +0600

    get single job description

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express') ;
 const cors = require('cors') ;
 require('dotenv').config() ;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000 ;
 
 const app = express() ;
@@ -34,8 +34,16 @@ async function run() {
   try {
      const jobCollection = client.db('worklyDB').collection('jobs') ;
     // await client.connect();
+
     app.get('/jobs', async(req,res)=> {
       const result = await jobCollection.find().toArray() ;
+      res.send(result) ;
+    })
+
+    app.get('/job/:id', async(req,res) => {
+      const id = req.params.id ;
+      const query = {_id : new ObjectId(id)}
+      const result = await jobCollection.findOne(query) ;
       res.send(result) ;
     })
 
