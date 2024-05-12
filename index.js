0563c63836e37bf4361f545b072c6c49ba8aa585
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-05-13 04:30:57 +0600

    fixed a minor bug

--- index.js
+++ index.js
@@ -79,12 +79,15 @@ async function run() {
     })
 
 
-
+  app.get('/jobs', async(req,res)=> {
+    const result = await jobCollection.find().toArray();
+    res.send(result);
+  })
 
     app.get('/jobs', async(req,res)=> {
       const search = req.query.search ;
       let query = {
-        job_title : {$regex : search, $options : 'i'}
+        job_title : {$regex : "search", $options : 'i'}
       }
       const result = await jobCollection.find(query).toArray() ;
       res.send(result) ;
